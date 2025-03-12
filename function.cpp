aa6bbfd958f0645cd53940b4c1e0ec69859cdd58
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-03-12 14:58:05 +0700

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -31,3 +31,20 @@ SDL_Texture* load_texture(SDL_Renderer* renderer, const char* image_file)
     }
     return texture;
 }
+
+SDL_Texture* load_texture_with_transparent_background(SDL_Renderer* renderer, const char* image_file)
+{
+    SDL_Surface* surface = IMG_Load(image_file);
+    if(surface == NULL)
+    {
+        cout << SDL_GetError();
+        return 0;
+    }
+/// make the white background transparent
+    SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, 255, 255, 255));
+
+/// the remaining parts
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    return texture;
+}
