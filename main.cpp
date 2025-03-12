8df15f1964a80d68e72c0b92c4d1b76f3b85182b
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-03-12 14:57:08 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ SDL_Window* window; // the main window
 SDL_Renderer* renderer; // window renderer
 SDL_Texture* background1; // background_1
 SDL_Texture* background2; // background_2
-
+SDL_Texture* character_left;
+SDL_Texture* character_right;
 
 const int WINDOW_WIDTH = 1400;
 const int WINDOW_HEIGHT = 750;
@@ -21,6 +22,7 @@ const int WINDOW_HEIGHT = 750;
 int main(int argc, char* argv[])
 {
     initializing();
+
 /// create window and renderer
     window = SDL_CreateWindow("SDL_Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     if(window == NULL)
@@ -38,12 +40,19 @@ int main(int argc, char* argv[])
         IMG_Quit();
         return 0;
     }
+
+/// background setup
     background1 = load_texture(renderer, "background/background1.png");
     background2 = load_texture(renderer, "background/background2.png");
+    int bg1_position_x = 0; // start position of background image 1
+    int bg2_position_x = WINDOW_WIDTH; // start position of background image 2
+
+/// character setup
+    character_left = load_texture_with_transparent_background(renderer, "Character/Basic_Character_Left.png");
+    character_right = load_texture_with_transparent_background(renderer, "Character/Basic_Character_Right.png");
+    const SDL_Rect character_position_rect = {572, 488, 256, 288};
 
 /// main loop run
-    int bg1_position_x = 0; // position of background image 1
-    int bg2_position_x = WINDOW_WIDTH; // position of background image 2
     bool run = true;
     SDL_Event e;
     while(run)
@@ -60,10 +69,16 @@ int main(int argc, char* argv[])
                 switch(e.key.keysym.sym)
                 {
                     case SDLK_LEFT:
-                        moving_background(1, bg1_position_x, bg2_position_x, renderer, background1, background2, WINDOW_WIDTH, WINDOW_HEIGHT);
+                        moving_background(0, bg1_position_x, bg2_position_x, renderer, background1, background2, WINDOW_WIDTH, WINDOW_HEIGHT);
+                        SDL_RenderCopy(renderer, character_left, NULL, &character_position_rect);
+                        SDL_RenderPresent(renderer);
+                        SDL_Delay(16);
                         break;
                     case SDLK_RIGHT:
-                        moving_background(0, bg1_position_x, bg2_position_x, renderer, background1, background2, WINDOW_WIDTH, WINDOW_HEIGHT);
+                        moving_background(1, bg1_position_x, bg2_position_x, renderer, background1, background2, WINDOW_WIDTH, WINDOW_HEIGHT);
+                        SDL_RenderCopy(renderer, character_right, NULL, &character_position_rect);
+                        SDL_RenderPresent(renderer);
+                        SDL_Delay(16);                      
                         break;
                 }
             }
