35c5c9424570a50aa42d208cdac3501d96e148a0
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-03-17 14:22:05 +0700

    Update background.cpp

--- background.cpp
+++ background.cpp
@@ -4,9 +4,8 @@
 #include <iostream>
 
 
-void moving_background(int left,double &bg1_position_x, double &bg2_position_x, SDL_Renderer* renderer, SDL_Texture* background1, SDL_Texture* background2, const int WINDOW_WIDTH, const int WINDOW_HEIGHT)
+void moving_background(int left,double &bg1_position_x, double &bg2_position_x, const int WINDOW_WIDTH, const int WINDOW_HEIGHT)
 {
-    SDL_RenderClear(renderer);
     const double BG_MOVING_SPEED = 5;
     if(left == 1)
     {
@@ -40,20 +39,4 @@ void moving_background(int left,double &bg1_position_x, double &bg2_position_x, 
             bg2_position_x = -WINDOW_WIDTH + BG_MOVING_SPEED;
         }
     }
-
-/// render both images
-    SDL_Rect window_rect;
-    window_rect.x = bg1_position_x;
-    window_rect.y = 0;
-    window_rect.w = WINDOW_WIDTH;
-    window_rect.h = WINDOW_HEIGHT;
-
-    
-    // render image 1
-    SDL_RenderCopy(renderer, background1, NULL, &window_rect);
-    
-    // render image 2
-    window_rect.x = bg2_position_x;
-    SDL_RenderCopy(renderer, background2, NULL, &window_rect);
-
 }
