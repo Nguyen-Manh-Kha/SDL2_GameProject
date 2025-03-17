2a388cafd4d556be39a7cc01c018234368c2ca05
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-03-17 14:23:07 +0700

    Update function.h

--- function.h
+++ function.h
@@ -6,7 +6,11 @@
 #include<SDL_image.h>
 
 void initializing();
+
 SDL_Texture* load_texture(SDL_Renderer*, const char*);
+
 SDL_Texture* load_texture_with_transparent_background(SDL_Renderer*, const char*);
+
+void update_character_frame(Uint32 &last_frame_time, int &frame_num, const int CHARACTER_ANIMATION_DELAY);
 
 #endif
