924ec73dcf675600284e6b18a7f7a3079e1c2c22
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-03-25 22:05:40 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "function.h"
 #include "background.h"
 #include "particlesystem.h"
+#include "treesystem.h"
 
 #include <iostream>
 #include <SDL.h>
@@ -19,17 +20,18 @@ SDL_Texture* basic_character_left[4]; // 3 basic_left_moving_sprites
 SDL_Texture* up_character_left[4]; // 3 up_left_moving_sprites
 SDL_Texture* basic_character_right[4]; // 3 basic_left_moving_sprites
 SDL_Texture* up_character_right[4]; // 3 up_right_moving_sprites
+SDL_Texture* tree_texture[19];
 
 const int WINDOW_WIDTH = 1400;
 const int WINDOW_HEIGHT = 750;
 
-map <SDL_Keycode, bool> key_press; // control key press state with no delay
+bool key_press[512]; // control key press state with no delay
 
 int main(int argc, char* argv[])
 {
     initializing();
 
-/// create window and renderer
+    ///// create window and renderer
     window = SDL_CreateWindow("SDL_Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     if(window == NULL)
     {
@@ -48,7 +50,7 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-/// background setup
+    ///// background setup
     background1 = load_texture(renderer, "background/background1.png");
     background2 = load_texture(renderer, "background/background2.png");
     double bg1_position_x = 0; // start position of background image 1
@@ -59,23 +61,47 @@ int main(int argc, char* argv[])
     background_rect.w = WINDOW_WIDTH;
     background_rect.h = WINDOW_HEIGHT;
 
-/// particle setup
-    particle_system P;
-    double particle_x, particle_y, particle_angle;
+    ///// forest setup
+    tree_texture[0] = NULL;
+    tree_texture[1] = load_texture(renderer, "Trees/tree_1.png");
+    tree_texture[2] = load_texture(renderer, "Trees/tree_2.png");
+    tree_texture[3] = load_texture(renderer, "Trees/tree_3.png");
+    tree_texture[4] = load_texture(renderer, "Trees/tree_4.png");
+    tree_texture[5] = load_texture(renderer, "Trees/tree_5.png");
+    tree_texture[6] = load_texture(renderer, "Trees/tree_6.png");
+    tree_texture[7] = load_texture(renderer, "Trees/tree_7.png");
+    tree_texture[8] = load_texture(renderer, "Trees/tree_8.png");
+    tree_texture[9] = load_texture(renderer, "Trees/tree_9.png");
+    tree_texture[10] = load_texture(renderer, "Trees/tree_10.png");
+    tree_texture[11] = load_texture(renderer, "Trees/tree_11.png");
+    tree_texture[12] = load_texture(renderer, "Trees/tree_12.png");
+    tree_texture[13] = load_texture(renderer, "Trees/tree_13.png");
+    tree_texture[14] = load_texture(renderer, "Trees/tree_14.png");
+    tree_texture[15] = load_texture(renderer, "Trees/tree_15.png");
+    tree_texture[16] = load_texture(renderer, "Trees/tree_16.png");
+    tree_texture[17] = load_texture(renderer, "Trees/tree_17.png");
+    tree_texture[18] = load_texture(renderer, "Trees/tree_18.png");
 
-/// character setup
-    basic_character_left[1] = load_texture_with_transparent_background(renderer, "Character/basic_character_left/Basic_Character_Left_1.png");
-    basic_character_left[2] = load_texture_with_transparent_background(renderer, "Character/basic_character_left/Basic_Character_Left_2.png");
-    basic_character_left[3] = load_texture_with_transparent_background(renderer, "Character/basic_character_left/Basic_Character_Left_3.png");
-    basic_character_right[1] = load_texture_with_transparent_background(renderer, "Character/basic_character_right/Basic_Character_Right_1.png");
-    basic_character_right[2] = load_texture_with_transparent_background(renderer, "Character/basic_character_right/Basic_Character_Right_2.png");
-    basic_character_right[3] = load_texture_with_transparent_background(renderer, "Character/basic_character_right/Basic_Character_Right_3.png");
-    up_character_left[1] = load_texture_with_transparent_background(renderer, "Character/up_character_left/Up_Character_Left_1.png");
-    up_character_left[2] = load_texture_with_transparent_background(renderer, "Character/up_character_left/Up_Character_Left_2.png");
-    up_character_left[3] = load_texture_with_transparent_background(renderer, "Character/up_character_left/Up_Character_Left_3.png");
-    up_character_right[1] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_1.png");
-    up_character_right[2] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_2.png");
-    up_character_right[3] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_3.png");
+    treesystem forest;
+    forest.setup_tree(tree_texture);
+
+    ///// particle setup
+    particle_system P;
+    double particle_x = 760, particle_y = 656, particle_angle = 0;
+
+    ///// character setup
+    basic_character_left[1] = load_texture_with_transparent_background(renderer, "Character/basic_character_left/Basic_Character_Left_1.png",255,255,255);
+    basic_character_left[2] = load_texture_with_transparent_background(renderer, "Character/basic_character_left/Basic_Character_Left_2.png",255,255,255);
+    basic_character_left[3] = load_texture_with_transparent_background(renderer, "Character/basic_character_left/Basic_Character_Left_3.png",255,255,255);
+    basic_character_right[1] = load_texture_with_transparent_background(renderer, "Character/basic_character_right/Basic_Character_Right_1.png",255,255,255);
+    basic_character_right[2] = load_texture_with_transparent_background(renderer, "Character/basic_character_right/Basic_Character_Right_2.png",255,255,255);
+    basic_character_right[3] = load_texture_with_transparent_background(renderer, "Character/basic_character_right/Basic_Character_Right_3.png",255,255,255);
+    up_character_left[1] = load_texture_with_transparent_background(renderer, "Character/up_character_left/Up_Character_Left_1.png",255,255,255);
+    up_character_left[2] = load_texture_with_transparent_background(renderer, "Character/up_character_left/Up_Character_Left_2.png",255,255,255);
+    up_character_left[3] = load_texture_with_transparent_background(renderer, "Character/up_character_left/Up_Character_Left_3.png",255,255,255);
+    up_character_right[1] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_1.png",255,255,255);
+    up_character_right[2] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_2.png",255,255,255);
+    up_character_right[3] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_3.png",255,255,255);
 
     bool move_left = false;
     bool move_right = false;
@@ -83,13 +109,16 @@ int main(int argc, char* argv[])
 
     int frame_num = 1;
 
-    const SDL_Rect character_rect = {572, 488, 256, 288};
+    const SDL_Rect character_rect = {618, 547, 192, 216};
 
     // control character frame-rate independently
     Uint32 last_frame_time = SDL_GetTicks();
     const int CHARACTER_ANIMATION_DELAY = 200;
+ 
+    // key state
+    key_press[512] = {false};
 
-/// main loop run
+    ///// main loop run
     bool run = true;
     SDL_Event e;
     while(run)
@@ -103,62 +132,66 @@ int main(int argc, char* argv[])
             
             if(e.type == SDL_KEYDOWN)
             {
-                key_press[e.key.keysym.sym] = true;
+                key_press[e.key.keysym.scancode] = true;
             }
 
             if(e.type == SDL_KEYUP)
             {
-                key_press[e.key.keysym.sym] = false;
+                key_press[e.key.keysym.scancode] = false;
             }
         }
 
         // handle key press
-        if(key_press[SDLK_LEFT] && key_press[SDLK_UP])
+        if(key_press[SDL_SCANCODE_LEFT] && key_press[SDL_SCANCODE_UP])
         {
             moving_background(0, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
             update_character_frame(last_frame_time, frame_num, CHARACTER_ANIMATION_DELAY);
             move_left = true; move_up = true; move_right = false;
-            particle_x = 640;
-            particle_y = 565;
+            forest.update_forest(move_left);
+            particle_x = 667;
+            particle_y = 604;
             particle_angle = 120;
         }
 
-        else if(key_press[SDLK_RIGHT] && key_press[SDLK_UP])
+        else if(key_press[SDL_SCANCODE_RIGHT] && key_press[SDL_SCANCODE_UP])
         {
             moving_background(1, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
             update_character_frame(last_frame_time, frame_num, CHARACTER_ANIMATION_DELAY);
             move_right = true; move_up = true; move_left = false;
-            particle_x = 758;
-            particle_y = 565;
+            forest.update_forest(move_left);
+            particle_x = 756;
+            particle_y = 608;
             particle_angle = 60;
         }
 
-        else if(key_press[SDLK_LEFT])
+        else if(key_press[SDL_SCANCODE_LEFT] && key_press[SDL_SCANCODE_UP] == false)
         {
             moving_background(0, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
             update_character_frame(last_frame_time, frame_num, CHARACTER_ANIMATION_DELAY);
             move_left = true; move_up = false; move_right = false;
-            particle_x = 630;
-            particle_y = 635;
+            forest.update_forest(move_left);
+            particle_x = 659;
+            particle_y = 655;
             particle_angle = 180;
         }
 
-        else if(key_press[SDLK_RIGHT])
+        else if(key_press[SDL_SCANCODE_RIGHT] && key_press[SDL_SCANCODE_UP] == false)
         {
             moving_background(1, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
             update_character_frame(last_frame_time, frame_num, CHARACTER_ANIMATION_DELAY);
             move_right = true; move_up = false; move_left = false;
-            particle_x = 767;
-            particle_y = 635;
+            forest.update_forest(move_left);
+            particle_x = 760;
+            particle_y = 656;
             particle_angle = 0;
         }
 
-        if(key_press[SDLK_a])
+        if(key_press[SDL_SCANCODE_A])
         {
             P.add_particle(particle_x, particle_y, particle_angle);
         }
 
-/// render everything
+    ///// render everything
     //  clear
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
@@ -169,8 +202,11 @@ int main(int argc, char* argv[])
         background_rect.x = bg2_position_x;
         SDL_RenderCopy(renderer, background2, NULL, &background_rect);
     
+    // render tree_system
+        forest.render_forest(renderer);
+
     // render water shooting
-    P.render_particle(renderer);
+        P.render_particle(renderer);
 
     // render character
         if(move_up)
@@ -186,7 +222,7 @@ int main(int argc, char* argv[])
         SDL_Delay(16);
     }
 
-/// Quit SDL
+    ///// Quit SDL
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_DestroyTexture(background1);
@@ -197,6 +233,10 @@ int main(int argc, char* argv[])
         SDL_DestroyTexture(basic_character_right[i]);
         SDL_DestroyTexture(up_character_left[i]);
         SDL_DestroyTexture(up_character_right[i]);
+    }
+    for(int i = 1; i <= 18; i++)
+    {
+        SDL_DestroyTexture(tree_texture[i]);
     }
     SDL_Quit();
     IMG_Quit();
