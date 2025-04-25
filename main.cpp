330777133e9cbae1a77a12d88ea0d089a994928e
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-04-26 00:43:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "particlesystem.h"
 #include "treesystem.h"
 #include "home.h"
+#include "character_status.h"
 
 #include <iostream>
 #include <SDL.h>
@@ -22,11 +23,16 @@ SDL_Texture* up_character_left[4]; // 3 up_left_moving_sprites
 SDL_Texture* basic_character_right[4]; // 3 basic_left_moving_sprites
 SDL_Texture* up_character_right[4]; // 3 up_right_moving_sprites
 SDL_Texture* tree_texture[19]; // 18 tree models
+SDL_Texture* burnt_tree_texture[19]; // 18 burnt tree models
 SDL_Texture* bush_texture[8]; // 7 bush models
+SDL_Texture* burnt_bush_texture[8]; // 7 burnt bush models
 SDL_Texture* flame_img[9]; // 8 flame sprites
-SDL_Texture* barrier[2]; // 2 barrier prites
+SDL_Texture* barrier[3]; // 2 barrier prites
 SDL_Texture* house_img;
 SDL_Texture* well_img;
+SDL_Texture* heart;
+SDL_Texture* water;
+SDL_Texture* menu;
 
 const int WINDOW_WIDTH = 1400;
 const int WINDOW_HEIGHT = 750;
@@ -71,7 +77,7 @@ int main(int argc, char* argv[])
     barrier[0] = NULL;
     barrier[1] = load_texture(renderer, "barrier/sign_left.png");
     barrier[2] = load_texture(renderer, "barrier/sign_right.png");
-    SDL_Rect barrier_rect = {0, 507, 177, 216};
+    SDL_Rect barrier_rect = {-13000, 507, 177, 216};
 
     house_img = load_texture(renderer, "house/house.png");
     house House(house_img);
@@ -102,9 +108,30 @@ int main(int argc, char* argv[])
     tree_texture[16] = load_texture(renderer, "Trees/tree_16.png");
     tree_texture[17] = load_texture(renderer, "Trees/tree_17.png");
     tree_texture[18] = load_texture(renderer, "Trees/tree_18.png");
+    
+    // burnt trees
+    burnt_tree_texture[0] = NULL;
+    burnt_tree_texture[1] = load_texture(renderer, "Burnt_tree/burnt_tree_1.png");
+    burnt_tree_texture[2] = load_texture(renderer, "Burnt_tree/burnt_tree_2.png");
+    burnt_tree_texture[3] = load_texture(renderer, "Burnt_tree/burnt_tree_3.png");
+    burnt_tree_texture[4] = load_texture(renderer, "Burnt_tree/burnt_tree_4.png");
+    burnt_tree_texture[5] = load_texture(renderer, "Burnt_tree/burnt_tree_5.png");
+    burnt_tree_texture[6] = load_texture(renderer, "Burnt_tree/burnt_tree_6.png");
+    burnt_tree_texture[7] = load_texture(renderer, "Burnt_tree/burnt_tree_7.png");
+    burnt_tree_texture[8] = load_texture(renderer, "Burnt_tree/burnt_tree_8.png");
+    burnt_tree_texture[9] = load_texture(renderer, "Burnt_tree/burnt_tree_9.png");
+    burnt_tree_texture[10] = load_texture(renderer, "Burnt_tree/burnt_tree_10.png");
+    burnt_tree_texture[11] = load_texture(renderer, "Burnt_tree/burnt_tree_11.png");
+    burnt_tree_texture[12] = load_texture(renderer, "Burnt_tree/burnt_tree_12.png");
+    burnt_tree_texture[13] = load_texture(renderer, "Burnt_tree/burnt_tree_13.png");
+    burnt_tree_texture[14] = load_texture(renderer, "Burnt_tree/burnt_tree_14.png");
+    burnt_tree_texture[15] = load_texture(renderer, "Burnt_tree/burnt_tree_15.png");
+    burnt_tree_texture[16] = load_texture(renderer, "Burnt_tree/burnt_tree_16.png");
+    burnt_tree_texture[17] = load_texture(renderer, "Burnt_tree/burnt_tree_17.png");
+    burnt_tree_texture[18] = load_texture(renderer, "Burnt_tree/burnt_tree_18.png");
 
     treesystem forest;
-    forest.setup_tree(tree_texture);
+    forest.setup_tree(tree_texture, burnt_tree_texture);
 
     // bushes
     bush_texture[0] = NULL;
@@ -116,8 +143,18 @@ int main(int argc, char* argv[])
     bush_texture[6] = load_texture(renderer, "Bush/bush_6.png");
     bush_texture[7] = load_texture(renderer, "Bush/bush_7.png");
 
+    // burnt bushes
+    burnt_bush_texture[0] = NULL;
+    burnt_bush_texture[1] = load_texture(renderer, "Burnt_bush/burnt_bush_1.png");
+    burnt_bush_texture[2] = load_texture(renderer, "Burnt_bush/burnt_bush_2.png");
+    burnt_bush_texture[3] = load_texture(renderer, "Burnt_bush/burnt_bush_3.png");
+    burnt_bush_texture[4] = load_texture(renderer, "Burnt_bush/burnt_bush_4.png");
+    burnt_bush_texture[5] = load_texture(renderer, "Burnt_bush/burnt_bush_5.png");
+    burnt_bush_texture[6] = load_texture(renderer, "Burnt_bush/burnt_bush_6.png");
+    burnt_bush_texture[7] = load_texture(renderer, "Burnt_bush/burnt_bush_7.png");
+
     bushsystem bushes;
-    bushes.setup_bush(bush_texture);
+    bushes.setup_bush(bush_texture, burnt_bush_texture);
 
     ///// flame sprites setup
     flame_img[0] = NULL;
@@ -147,6 +184,13 @@ int main(int argc, char* argv[])
     up_character_right[1] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_1.png",255,255,255);
     up_character_right[2] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_2.png",255,255,255);
     up_character_right[3] = load_texture_with_transparent_background(renderer, "Character/up_character_right/Up_Character_Right_3.png",255,255,255);
+    
+    heart = load_texture(renderer, "Character/heart.png");
+    water = load_texture(renderer, "Character/h2o.png");
+
+    status character_status(heart, water);
+    int survive_tree = 0;
+    int survive_bush = 0;
 
     bool move_left = false;
     bool move_right = false;
@@ -169,6 +213,7 @@ int main(int argc, char* argv[])
     ///// main loop run
     bool run = true;
     SDL_Event e;
+
     while(run)
     {
         while(SDL_PollEvent(&e) != 0)
@@ -193,42 +238,45 @@ int main(int argc, char* argv[])
         if(current_frame_time - last_fire_time >= 200)
         {
             forest.spread_flame_forest();
+            forest.health_tree();
+            
             bushes.spread_flame_bushes();
+            bushes.health_bush();
+            
             House.spread_flame_house(forest, bushes);
+
+            character_status.catch_fire(forest, bushes);
+            character_status.on_fire();
+
+            if(House.lose_health())
+            {
+                cout << "You lose" << endl << "The fire destroyed your house";
+                run = false;
+            }
             Well.spread_flame_well(bushes);
+            if(Well.lose_health())
+            {
+                cout << "You lose" << endl << "The fire destroyed the well";
+                run = false;
+            }
+            if(forest.lose() && bushes.lose())
+            {
+                cout << "You lose" << endl << "The fire destroyed the forest";
+            }
+            if(character_status.lose())
+            {
+                cout << "You lose" << endl << "You lost all your health. Beware of the fire";
+            }
+            if(forest.win(survive_tree) && bushes.win(survive_bush))
+            {
+                int percentage = (survive_tree + survive_bush) / (forest.tree_system.size() + bushes.bush_system.size());
+                cout << "You win" << endl << "You saved " << percentage << '%' << " of the forest";
+            }
             last_fire_time = current_frame_time;
         }
 
     ///// handle key press
-        if(key_press[SDL_SCANCODE_LEFT] && key_press[SDL_SCANCODE_UP])
-        {
-            moving_background(0, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
-            update_character_frame(last_character_time, frame_num, CHARACTER_ANIMATION_DELAY);
-            move_left = true; move_up = true; move_right = false;
-            forest.update_forest(move_left);
-            bushes.update_bushes(move_left);
-            House.update_position(move_left);
-            Well.update_position(move_left);
-            particle_x = 667;
-            particle_y = 604;
-            particle_angle = 115;
-        }
-
-        else if(key_press[SDL_SCANCODE_RIGHT] && key_press[SDL_SCANCODE_UP])
-        {
-            moving_background(1, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
-            update_character_frame(last_character_time, frame_num, CHARACTER_ANIMATION_DELAY);
-            move_right = true; move_up = true; move_left = false;
-            forest.update_forest(move_left);
-            bushes.update_bushes(move_left);
-            House.update_position(move_left);
-            Well.update_position(move_left);
-            particle_x = 756;
-            particle_y = 608;
-            particle_angle = 65;
-        }
-
-        if(key_press[SDL_SCANCODE_LEFT])
+        if(key_press[SDL_SCANCODE_LEFT] && barrier_rect.x <= 400)
         {
             moving_background(0, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
             update_character_frame(last_character_time, frame_num, CHARACTER_ANIMATION_DELAY);
@@ -237,12 +285,13 @@ int main(int argc, char* argv[])
             bushes.update_bushes(move_left);
             House.update_position(move_left);
             Well.update_position(move_left);
+            barrier_rect.x += 5;
             particle_x = 659;
             particle_y = 655;
             particle_angle = 170;
         }
 
-        else if(key_press[SDL_SCANCODE_RIGHT])
+        else if(key_press[SDL_SCANCODE_RIGHT] && barrier_rect.x >= -27100)
         {
             moving_background(1, bg1_position_x, bg2_position_x, WINDOW_WIDTH, WINDOW_HEIGHT);
             update_character_frame(last_character_time, frame_num, CHARACTER_ANIMATION_DELAY);
@@ -251,14 +300,39 @@ int main(int argc, char* argv[])
             bushes.update_bushes(move_left);
             House.update_position(move_left);
             Well.update_position(move_left);
+            barrier_rect.x -= 5;
             particle_x = 760;
             particle_y = 656;
             particle_angle = 10;
         }
 
-        if(key_press[SDL_SCANCODE_A])
+        if(move_left && key_press[SDL_SCANCODE_UP])
+        {
+            move_left = true; move_right = false; move_up = true;
+            particle_x = 667;
+            particle_y = 604;
+            particle_angle = 115;
+        }
+
+        else if(move_right && key_press[SDL_SCANCODE_UP])
+        {
+            move_left = false; move_right = true; move_up = true;
+            particle_x = 756;
+            particle_y = 608;
+            particle_angle = 65;
+        }
+
+        if(key_press[SDL_SCANCODE_A] && character_status.not_empty_water())
         {
             P.add_particle(particle_x, particle_y, particle_angle);
+            key_press[SDL_SCANCODE_R] = false;
+            character_status.lose_water();
+        }
+        
+        if(key_press[SDL_SCANCODE_R] && character_status.not_full_water() && Well.near_character())
+        {
+            key_press[SDL_SCANCODE_A] = false;
+            character_status.refill_water();
         }
 
     ///// render everything
@@ -272,6 +346,12 @@ int main(int argc, char* argv[])
         background_rect.x = bg2_position_x;
         SDL_RenderCopy(renderer, background2, NULL, &background_rect);
     
+    // render map barrier
+        SDL_RenderCopy(renderer, barrier[1], NULL, &barrier_rect);
+        barrier_rect.x += 28000;
+        SDL_RenderCopy(renderer, barrier[2], NULL, &barrier_rect);
+        barrier_rect.x -= 28000;
+
     // render home
         House.render_house(renderer, flame_img);
     
@@ -294,6 +374,8 @@ int main(int argc, char* argv[])
         else if(move_left) SDL_RenderCopy(renderer, basic_character_left[frame_num], NULL, &character_rect);
         else SDL_RenderCopy(renderer, basic_character_right[frame_num], NULL, &character_rect);
 
+        character_status.render(renderer, flame_img);
+
     // present
         SDL_RenderPresent(renderer);
         SDL_Delay(16);
@@ -309,7 +391,13 @@ int main(int argc, char* argv[])
     for(int i = 1; i <= 18; i++)
     {
         SDL_DestroyTexture(tree_texture[i]);
+        SDL_DestroyTexture(burnt_bush_texture[i]);
         if(i <= 8) SDL_DestroyTexture(flame_img[i]);
+        if(i <= 7)
+        {
+            SDL_DestroyTexture(bush_texture[i]);
+            SDL_DestroyTexture(burnt_bush_texture[i]);
+        }
         if(i <= 3)
         {
             SDL_DestroyTexture(basic_character_left[i]);
